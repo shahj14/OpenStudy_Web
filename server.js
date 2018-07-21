340446aaf8c976589012c0a1e0fa91fb9801d29e
Korey Huskonen <huskonkw@mail.uc.edu>
2018-07-21 17:08:17 -0400

    api testing

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const path = require('path')
+const cool = require('cool-ascii-faces')
+const PORT = process.env.PORT || 5000
+const { Pool } = require('pg')
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  // ssl: true
+})
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  // .set('views', path.join(__dirname, 'views'))
+  // .set('view engine', 'ejs')
+  // .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .get('/times', (req, res) => {
+      let result = ''
+      const times = process.env.TIMES || 5
+      for (i = 0; i < times; i++) {
+        result += i + ' '
+      }
+      res.send(result)
+  })
+  .get('/db', async (req, res) => {
+      try {
+        const client = await pool.connect()
+        const result = await client.query('SELECT * FROM student');
+        res.send(result);
+        client.release();
+      } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+      }
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
