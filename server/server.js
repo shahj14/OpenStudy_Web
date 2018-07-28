487040bcf9480e1cc4ee207c7374b87a4d129830
Korey Huskonen <huskonkw@mail.uc.edu>
2018-07-28 19:00:47 -0400

    Initial API commit

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 const express = require('express')
+var bodyParser = require("body-parser")
 const cool = require('cool-ascii-faces')
 const PORT = process.env.PORT || 5100
 const { Pool } = require('pg')
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL || "postgres://iutizeynjekqsj:07edc994d8d267f257d7be8c46b335ad43b2829d09bac5b2e1ebd80f6c0deadb@ec2-184-72-219-186.compute-1.amazonaws.com:5432/d7qpg7fsl42tkc",
   ssl: true
 })
+
 const app = express();
+app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('../client/build'));
@@ -14,10 +17,12 @@ if (process.env.NODE_ENV === 'production') {
 
 app.get('/cool', (req, res) => res.send(cool()))
 
-app.get('/db', async (req, res) => {
+// get all rooms on a floor
+app.get('/api/floor/:num', async (req, res) => {
     try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM room');
+      const num = req.params.num;
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM room WHERE floor=$1', [num]);
       res.send(result.rows);
       client.release();
     } catch (err) {
@@ -25,5 +30,74 @@ app.get('/db', async (req, res) => {
       res.send('=> ' + err);
     }
 })
+
+// get room by number
+ app.get('/api/room/:num', async (req, res) => {
+     try {
+       const num = req.params.num;
+       const client = await pool.connect();
+       const result = await client.query('SELECT * FROM room WHERE number=$1', [num]);
+       res.send(result.rows[0]);
+       client.release();
+     } catch (err) {
+       console.error(err);
+       res.send('=> ' + err);
+     }
+ })
+
+// flip the occupied value of a room
+app.get('/api/room/:num/true', async (req, res) => {
+     try {
+       const num = req.params.num;
+       const client = await pool.connect();
+       const result = await client.query('UPDATE room SET occupied=true WHERE number=$1 RETURNING *', [num]);
+       res.send(result.rows[0]);
+       client.release();
+     } catch (err) {
+       console.error(err);
+       res.send('=> ' + err);
+     }
+ })
+
+ app.get('/api/room/:num/false', async (req, res) => {
+      try {
+        const num = req.params.num;
+        const client = await pool.connect();
+        const result = await client.query('UPDATE room SET occupied=false WHERE number=$1 RETURNING *', [num]);
+        res.send(result.rows[0]);
+        client.release();
+      } catch (err) {
+        console.error(err);
+        res.send('=> ' + err);
+      }
+  })
+
+ app.get('/api/user', async (req, res) => {
+     try {
+       const num = req.params.num;
+       const client = await pool.connect();
+       const result = await client.query('SELECT * FROM student');
+       res.send(result.rows);
+       client.release();
+     } catch (err) {
+       console.error(err);
+       res.send('=> ' + err);
+     }
+ })
+
+ app.post('/api/user', async (req, res) => {
+     try {
+       var b = req.body;
+       const client = await pool.connect();
+       const result = await client.query('INSERT INTO student VALUES($1, $2, $3, $4) RETURNING *', [b.email, b.fname, b.lname, b.email]);
+       res.send(result.rows[0]);
+       client.release();
+     } catch (err) {
+       console.error(err);
+       res.send('=> ' + err);
+     }
+ })
+
+
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
